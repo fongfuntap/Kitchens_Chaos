2cb296df51337145692db38d09a905bdcd266390
Phong2512 <phongpg@funtap.vn>
2024-08-14 17:42:24 +0700

    feat: Handle ClearCouter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,32 +6,93 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
+    private ClearCounter selectCounter;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (selectCounter != null) { 
+            selectCounter.Interact();
+        }
+    }
+
     private void Update()
+    {
+       HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-        Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float moveDistance = moveSpeed*Time.deltaTime;
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance,counterLayerMask)) { 
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                /*clearCounter.Interact();*/
+                if (clearCounter != selectCounter)
+                {
+                    selectCounter = clearCounter;
+                }
+                else
+                {
+                    selectCounter = null;
+                }
+            }
+        }
+        else
+        {
+            selectCounter = null;
+        }
+       
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = .7f;
         float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(this.transform.position,this.transform.position + Vector3.up*playerHeight ,playerRadius,moveDir,moveDistance);
+        bool canMove = !Physics.CapsuleCast(this.transform.position, this.transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
         if (!canMove)
         {
-            Vector3 moveDirX = new Vector3(moveDir.x,0f,0f).normalized;
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
             canMove = !Physics.CapsuleCast(this.transform.position, this.transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
 
-            if (canMove) { 
+            if (canMove)
+            {
                 moveDir = moveDirX;
             }
             else
             {
-                Vector3 moveDirZ = new Vector3(0f,0f, moveDir.z).normalized;
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(this.transform.position, this.transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
-                if (canMove) { 
+                if (canMove)
+                {
                     moveDir = moveDirZ;
                 }
             }
@@ -41,15 +102,10 @@ public class Player : MonoBehaviour
         {
             this.transform.position += moveDir * moveDistance;
         }
-        
+
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
-        this.transform.forward = Vector3.Slerp(this.transform.forward,moveDir,Time.deltaTime*rotateSpeed);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
+        this.transform.forward = Vector3.Slerp(this.transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
